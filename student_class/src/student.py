7ce5a62e3886efb933ec9416be33802ec4f7d5ec
Zuhayr Khan <zuhayrkhan@Zuhayrs-MacBook-Air.local>
2023-05-22 16:59:37 +0100

    A & B & Extensions Done

---
+++ student_class/src/student.py
@@ -0,0 +1,11 @@
+class Student:
+    def __init__(self, student_name, cohort):
+        self.name = student_name
+        self.cohort = cohort
+
+    def talk(self):
+        return('I can talk!')
+    
+    def say_favourite_language(self, language):
+        language = 'Python'
+        return('I love ' + language)
